e6ce307d1e0369e747272db9767ee63fb73f05d2
fnikol <fnikol@ics.forth.gr>
2022-04-29 22:28:54 +0300

    update distro

--- src/ubuntu/install/icewm_ui.sh
+++ src/ubuntu/install/icewm_ui.sh
@@ -3,7 +3,5 @@
 set -e
 
 echo "Install IceWM UI components"
-apt-get update 
 apt-get install -y supervisor icewm xterm xfonts-base xauth xinit
 apt-get purge -y pm-utils xscreensaver*
-apt-get clean -y
